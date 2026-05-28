6e7b5feb12f5a3d7d0b64c1d2facbdacaa2cc738
przybyla-nikodem <przybylanikodem@gmail.com>
2026-05-28 23:36:15 +0200

    pliki

---
+++ Models/User.cs
@@ -0,0 +1,43 @@
+﻿namespace API___ASP_.NET_Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public byte[] passwordSalt { get; set; }
+        public string Birthday { get; set; }
+
+    }
+
+    public class Register
+    {
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Birthday { get; set; }
+
+    }
+
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Edit
+    {
+        public string Username { get; set; }
+        public string newUsername { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Birthday { get; set; }
+    }
+}
